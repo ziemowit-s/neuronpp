187d561f6e6b613c2e3cf0bdea72d681c43b0580
Ziemowit S <ziemowit.slawinski@gmail.com>
2020-01-20 11:16:28 +0100

    added simple spines with synapses to Combe2018 model

--- neuronpp/examples/combe2018_example.py
+++ neuronpp/examples/combe2018_example.py
@@ -1,8 +1,13 @@
+from neuronpp.utils.utils import make_shape_plot
+
 from neuronpp.cells.combe2018_cell import Combe2018Cell
 from neuronpp.utils.run_sim import RunSim
 
 
 cell = Combe2018Cell(name="cell", compile=True)
-sim = RunSim()
+syns, heads = cell.make_spine_with_synapse(source=None, weight=0.01, number=1000, mod_name="ExpSyn", delay=1, sec="apic")
+
+make_shape_plot()
+sim = RunSim(with_neuron_gui=True)
 sim.run(runtime=500)
 
