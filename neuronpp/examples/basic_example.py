d9ee5d7e6148b88df8bc82e58088175ce10fb641
Ziemowit S <ziemowit.slawinski@gmail.com>
2020-01-20 21:36:31 +0100

    minor

--- neuronpp/examples/basic_example.py
+++ neuronpp/examples/basic_example.py
@@ -8,15 +8,15 @@ from neuronpp.utils.utils import make_shape_plot
 from neuronpp.core.cells.netstim_cell import NetStimCell
 
 cell = Cell(name="cell")
-cell.load_morpho(filepath='commons/morphologies/swc/my.swc', seg_per_L_um=1, make_const_segs=11)
+cell.load_morpho(filepath='commons/morphologies/asc/cell2.asc')
 cell.insert("pas")
 cell.insert("hh")
 
 SYNAPSE_MECH = "ExpSyn"
-stim = NetStimCell("stim_cell").make_netstim(start=300, number=5, interval=10)
+stim = NetStimCell("stim_cell").make_netstim(start=30, number=5, interval=10)
 
 syn1 = cell.make_spine_with_synapse(source=stim, weight=0.01, mod_name=SYNAPSE_MECH,
-                                    sec="dend", delay=1, head_nseg=10, neck_nseg=10, number=10)[0]
+                                    sec="apic", delay=1, head_nseg=1, neck_nseg=1, number=100)[0]
 
 ic = IClamp(segment=cell.filter_secs("soma")[0].hoc(0.5))
 ic.stim(delay=100, dur=10, amp=0.1)
@@ -26,11 +26,13 @@ rec_v = Record(cell.filter_secs(name="soma"), locs=0.5, variables="v")
 
 # Spike detector
 cell.make_spike_detector(sec="soma", loc=0.5)
-make_shape_plot()
+ps = make_shape_plot()
 
 # run
-sim = RunSim(init_v=-65, warmup=20, with_neuron_gui=True)
-sim.run(runtime=500)
+sim = RunSim(init_v=-65, warmup=20, with_neuron_gui=True, shape_plots=[ps])
+
+print('Run..')
+sim.run(runtime=200)
 
 # plot
 cell.plot_spikes()
