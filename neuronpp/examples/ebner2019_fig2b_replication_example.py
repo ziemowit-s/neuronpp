4be6176635f59e0329b37684ecb91ab7125c9832
Ziemowit S <ziemowit.slawinski@gmail.com>
2020-01-29 21:46:04 +0100

    fixed Ebner2019 example

--- neuronpp/examples/ebner2019_fig2b_replication_example.py
+++ neuronpp/examples/ebner2019_fig2b_replication_example.py
@@ -14,28 +14,33 @@ COOL_DOWN = 100
 WEIGHT = 0.0035
 
 if __name__ == '__main__':
-    freq = 10
+    freq = 50  # freqs.append(10, 20, 40, 50)
+    delta_t = 10  # LTP
+    #delta_t = -10  # LTD
 
     # Cell definition
     cell = HocCell("cell", compile_paths="../commons/mods/hay2011 ../commons/mods/ebner2019")
     cell.load_hoc("../commons/hocmodels/ebner2019/load_model.hoc", cell_template_name="L5PCtemplate")
 
+    interval = 1000/freq
     # Netstim to synapse
     stim = NetStimCell("stim")
-    st = stim.make_netstim(start=WARMUP, number=REPS, interval=1000/freq)
+    st = stim.make_netstim(start=WARMUP, number=REPS, interval=interval)
     cell.make_netcons(source=st, weight=WEIGHT, mod_name="Syn4P")
 
     # IClamp to soma
     iclamp = IClamp(segment=cell.filter_secs("soma")[0].hoc(0.5))
-    iclamp.stim(delay=0, dur=DUR, amp=AMP)
+    for i in range(REPS):
+        start_t = delta_t + i * interval + WARMUP
+        iclamp.stim(delay=start_t, dur=DUR, amp=AMP)
 
     # Record
     rec = Record(cell.filter_secs("apic[1]")+cell.filter_secs("apic[50]"), loc=0.5)
 
     # Run
-    total_time = WARMUP + REPS * (1000.0 / freq) + COOL_DOWN
+    total_time = WARMUP + REPS * interval + COOL_DOWN
     sim = RunSim(init_v=-70, warmup=WARMUP, dt=DT)
-    sim.run(1)
+    sim.run(total_time)
 
     # Plot
-    rec.plot(position="grid")
+    rec.plot(position="merge")
