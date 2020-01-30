1d0e75292b505d970a86dc6713f49cc2da59fc7c
Ziemowit Slawinski <ziemowit.slawinski@gmail.com>
2020-01-30 12:08:32 +0100

    fixed warmuptime

--- neuronpp/examples/ebner2019_fig2b_replication_example.py
+++ neuronpp/examples/ebner2019_fig2b_replication_example.py
@@ -16,7 +16,6 @@ WEIGHT = 0.0035
 if __name__ == '__main__':
     freq = 50  # freqs.append(10, 20, 40, 50)
     delta_t = 10  # LTP
-    #delta_t = -10  # LTD
 
     # Cell definition
     cell = HocCell("cell", compile_paths="../commons/mods/hay2011 ../commons/mods/ebner2019")
@@ -38,8 +37,8 @@ if __name__ == '__main__':
     rec = Record(cell.filter_secs("apic[1]")+cell.filter_secs("apic[50]"), loc=0.5)
 
     # Run
-    total_time = WARMUP + REPS * interval + COOL_DOWN
     sim = RunSim(init_v=-70, warmup=WARMUP, dt=DT)
+    total_time = REPS * interval + COOL_DOWN
     sim.run(total_time)
 
     # Plot
