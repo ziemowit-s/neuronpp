5b26e76d474048984c2c2041bf98b64e1925a15e
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2020-03-12 16:38:51 +0100

    Add spines to chosen parts (by name) of the dendritic tree

--- neuronpp/core/cells/spine_cell.py
+++ neuronpp/core/cells/spine_cell.py
@@ -151,3 +151,103 @@ class SpineCell(SectionCell):
               neck dimensions
         """
 
+    def add_spines_to_regions(self, regions, spine_density, spine_type,
+                              **kwargs):
+        """
+        Add spines with specified longitudal density (per 1 um) to a part
+        of dendritic range specified as distance from the soma. Spines can have
+        a predifined type (stubby, thin, mushroom) or, alternatively, their
+        dimentions (head_diam, head_len, neck_diam, neck_len) can be specified
+        in kwargs.
+
+        :param regions:
+            Sections that will have spines
+        :param spine_density:
+            spine density 
+        :param spine_type:
+            Spine type. There are four predifined types: thin, stubby,
+            mushroom and other.
+        :param \**kwargs:
+            See below
+
+        Keyword arguments:
+        :head_diam:
+            Spine head diameter
+        :head_len:
+            Length of the spine head
+        :neck_diam:
+            Spine neck diameter
+        :neck_len:
+            Length of the spine neck
+        :seed: None
+            seed for the random number generator used for picking out
+            spine positions
+        :area_densisty:
+            if False spine_density is treated as longitudal spine density [um]
+            if True  spine_density is treated as area density [um2]
+        :return:
+            list of added spine heads
+
+        TODO: add spines with a distribution of head dimensions and
+              neck dimensions
+        """
+        if not isinstance(regions, list):
+            regions = [regions]
+        try:
+            spine_dimensions = SPINE_DIMENSIONS[spine_type]
+        except KeyError
+            spine_dimensions = SPINE_DIMENSIONS["generic"]
+
+        head_diam = kwargs.pop("head_diam", spine_dimesions["head_diam"])
+        head_len = kwargs.pop("head_len", spine_dimesions["head_len"])
+        neck_diam = kwargs.pop("neck_diam", spine_dimesions["neck_diam"])
+        neck_len = kwargs.pop("neck_len", spine_dimesions["neck_len"])
+        seed = kwargs.pop("seed", None)
+        area_density = kwargs.pop("area_density", False)
+
+        if isinstance(seed, int):
+            random.seed(seed)
+
+        sections = self.get_sections_to_add_spines(regions)
+        for sec in sections:
+            name = sec.name()
+            if area_density:
+                area = sec.L*np.pi*sec.diam
+                spine_number = int(np.round(area * spine_density))
+            else:
+                spine_number = int(np.round(sec.L * spine_density))
+
+            for i in range(spine_number):
+                head = self.add_sec(name="%s_head[%d]" % (name, i),
+                                    diam=head_diam,
+                                    l=head_len, nseg=2)
+                neck = self.add_sec(name="%s_neck[%d]" % (name, i),
+                                    diam=neck_diam,
+                                    l=neck_len, nseg=1)
+                self.heads.append(head)
+                self.necks.append(neck)
+                self.connect_secs(source=head, target=neck)
+                if isinstance(seed, int):
+                    self._connect_necks_rand_uniform(neck, [sec])
+                else:
+                    self._connect_spines(neck, sec, i, spine_number)
+
+        return self.heads, self.necks
+
+    def _get_sections_to_add_spines(self, region_list):
+        """
+        Go through all the sections and pick ones with names
+        starting with strings from region_list
+        :param region_list:
+            list of region names
+
+        :returns
+            list of sections
+        """
+        section_list = []
+        for region in regions:
+            for section in self.sec:
+                if section.name().startswith(region) \
+                   and section not in section_list:
+                    section_list.append(section)
+        return section_list
