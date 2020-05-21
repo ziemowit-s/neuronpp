779c9f4d2361ee0e0495ceec9d5568f7d6456346
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2020-05-21 14:28:11 +0200

    Correct path to model

--- neuronpp/cells/combe2018_cell.py
+++ neuronpp/cells/combe2018_cell.py
@@ -3,7 +3,7 @@ import os
 from neuronpp.cells.cell import Cell
 from neuronpp.core.cells.core_hoc_cell import CoreHocCell
 path = os.path.dirname(os.path.abspath(__file__))
-f_path = os.path.join(path, "..", "commons/mods/combe2018")
+f_path = os.path.join(path, "..", "commons/hocmodels/combe2018")
 
 class Combe2018Cell(Cell, CoreHocCell):
     def __init__(self, name=None, model_folder=f_path, spine_number=0,
