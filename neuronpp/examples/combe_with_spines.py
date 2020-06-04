1e52c0c3d52debe8e3ceead3368c11f74e3c1837
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2020-06-04 13:44:27 +0200

    Make IV curves for Combe

--- neuronpp/examples/combe_with_spines.py
+++ neuronpp/examples/combe_with_spines.py
@@ -6,25 +6,47 @@ from neuronpp.utils.simulation import Simulation
 from neuronpp.utils.record import Record
 from neuronpp.utils.iclamp import IClamp
 # Create cell
-cell = Combe2018Cell(name="cell1")
 h.CVode()
-soma = cell.filter_secs("soma")
-axon = cell.filter_secs("axon")
+h.CVode().active(True)
 
+cell = Combe2018Cell(name="cell1")
+soma = cell.filter_secs("soma")
 
 fig, ax = plt.subplots(1, 1)
-injections = [-.1, 0, .100]
+injections = [-.1, 0, .100, .200]
 
 for inj in injections:
     print(inj)
-    ic = IClamp(segment=soma(0.5))
+    ic = h.IClamp(soma.hoc(0.5))
     rec_v = h.Vector().record(soma.hoc(0.5)._ref_v)
     time =  h.Vector().record(h._ref_t)
-    ic.stim(delay=300, dur=1000, amp=inj)
-    sim = Simulation(init_v=-70, constant_timestep=False)
-    sim.run(runtime=1500, stepsize=1)
+    ic.delay = 300
+    ic.dur = 1000
+    ic.amp = inj
+    h.finitialize(-70)
+    h.fcurrent()
+    h.tstop = 1500
+    h.run()
     ax.plot(time, rec_v, label="%4.3fnA" % inj)
-    #sim.reset()
+
+f1 = open("combe_mine_apic.txt", "w")
+f2 = open("combe_mine_trunk.txt", "w")
+f3 = open("combe_mine_dend.txt", "w")
+f4 = open("combe_mine_sa.txt", "w")
+for sec in cell.secs:
+    if "trunk"  in sec.hoc.name():
+        f = f2
+    elif "apic" in sec.hoc.name():
+        f = f1
+    elif "dend" in sec.hoc.name():
+        f = f3
+    else:
+        f = f4
+
+    mechs = sec.hoc.psection()["density_mechs"]
+    for key, value in mechs.items():
+        f.write("%s %s " %(sec.hoc.name(), key) + str(value) + "\n")
+
 ax.set_xlabel("time (s)")
 ax.set_ylabel("V (mV)")
 ax.legend()
