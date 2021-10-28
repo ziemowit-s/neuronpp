Added parallel computation to the Simulation object; During atribute filtering in the CoreCell - fixed bug: now it will continue filtering loop while Attribute not found; added returning of spike detector to the NetConCell; fixed setup of PointProcess parent segment in the PointProcessCell; Added get_records_by_time method to the RecordOutput object - where you can specify records you want to obtain between 2 boundaries; fixed get_records_to_time in the RecordOutput object - to properly return the last record in the list; changed docstrings in the RecordOutput; added tests for specified methods in the RecordOutput; added load_mods function to the utils

neuronpp/utils/RecordOutput.py
@@ -19,7 +19,7 @@ class RecordOutput:
 
     def get_records_from_time(self, ms):
         """
-        Returns records array where time in miliseconds is higher than ms.
+        Returns records array where time in miliseconds is equal or higher than ms.
 
         If there were multiple sources of recordings (eg. many segments or many point processes)
         each of them will be stored as a separated dimension of the array.
@@ -27,17 +27,42 @@ class RecordOutput:
         :param ms:
             time in ms
         """
-        min_arg = np.min(np.where(self.time > ms))
+        min_arg = np.min(np.where(self.time >= ms))
         return self.records[min_arg:]
 
     def get_records_to_time(self, ms):
         """
         Returns records array where time in miliseconds is lower than ms
+
+        If there were multiple sources of recordings (eg. many segments or many point processes)
+        each of them will be stored as a separated dimension of the array.
+
         :param ms:
             time in ms
         """
-        max_arg = np.max(np.where(self.time > ms))
+        max_arg = np.max(np.where(self.time <= ms))+1
         return self.records[:max_arg]
+
+    def get_records_by_time(self, from_ms, to_ms, with_time_vector=False):
+        """
+        Returns records array where time in miliseconds is between 2 values
+
+        If there were multiple sources of recordings (eg. many segments or many point processes)
+        each of them will be stored as a separated dimension of the array.
+
+        :param to_ms:
+            time in ms, inclusive (will be higher or equal this time)
+        :param from_ms:
+            time in ms, exclusive (will be lower than this time)
+        :param with_time_vector:
+            Default False. If True it will return tuple of (records, time)
+        """
+        from_arg = np.min(np.where(self.time >= from_ms))
+        to_arg = np.max(np.where(self.time <= to_ms))+1
+        if with_time_vector:
+            return self.records[from_arg:to_arg], self.time[from_arg:to_arg]
+        else:
+            return self.records[from_arg:to_arg]
 
     @property
     def size(self):